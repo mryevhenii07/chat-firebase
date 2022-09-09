435d9022b404c1e47149da413c62356b3b2bc446
mryevhenii07 <mrit.works07@gmail.com>
2022-09-09 21:57:43 +0300

    create firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import './App.css';
+import React from "react";
+import NavBar from "./component/NavBar";
 
+const style = {
+  appContainer: "max-w-[728px] mx-auto text-center",
+  sectionContainer:
+    "flex flex-col h-[90vh] bg-gray=100 mt-10 shadow-xl border relative",
+};
 function App() {
-  return <div>ff</div>;
+  return (
+    <div className={style.appContainer}>
+      <section className={style.sectionContainer}>
+        <NavBar />
+      </section>
+    </div>
+  );
 }
 
 export default App;
